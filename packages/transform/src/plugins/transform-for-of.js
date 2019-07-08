16a842d592bc0552acd5847bac831aa040b865f0
Frankie Dintino <fdintino@gmail.com>
2019-07-08 02:13:37 -0400

    Use babel7 transform-for-of, add tests

--- packages/transform/src/plugins/transform-for-of.js
+++ packages/transform/src/plugins/transform-for-of.js
@@ -1,157 +1,128 @@
 export default function transformForOfPlugin({types: t}) {
-  function buildForOfArray({KEY, ARR, BODY}) {
-    return t.forStatement(
-      t.variableDeclaration('var', [t.variableDeclarator(KEY, t.numericLiteral(0))]),
-      t.binaryExpression('<', KEY, t.memberExpression(ARR, t.identifier('length'))),
-      t.updateExpression('++', KEY, false),
-      (t.isStatement(BODY)) ? BODY : t.expressionStatement(BODY)
-    );
-  }
+  const memb = t.memberExpression;
+  const ident = t.identifier;
 
-  function buildForOfLoose({LOOP_OBJECT, IS_ARRAY, OBJECT, INDEX, ID}) {
+  function buildForOfLoose({loopObj, obj, isArr, index, intermediate, id}) {
     return t.forStatement(
       t.variableDeclaration('var', [
-        t.variableDeclarator(LOOP_OBJECT, OBJECT),
+        t.variableDeclarator(loopObj, obj),
         t.variableDeclarator(
-          IS_ARRAY,
-          t.callExpression(
-            t.memberExpression(t.identifier('Array'), t.identifier('isArray')),
-            [LOOP_OBJECT])),
-        t.variableDeclarator(INDEX, t.numericLiteral(0)),
+          isArr,
+          t.callExpression(memb(ident('Array'), ident('isArray')), [loopObj])),
+        t.variableDeclarator(index, t.numericLiteral(0)),
         t.variableDeclarator(
-          LOOP_OBJECT,
+          loopObj,
           t.conditionalExpression(
-            IS_ARRAY,
-            LOOP_OBJECT,
+            isArr,
+            loopObj,
             t.callExpression(
-              t.memberExpression(
-                LOOP_OBJECT,
-                t.memberExpression(t.identifier('Symbol'), t.identifier('iterator')),
-                true),
-              []))),
+              memb(loopObj, memb(ident('Symbol'), ident('iterator')), true),
+              [])))
       ]),
       null,
       null,
       t.blockStatement([
-        t.variableDeclaration('var', [t.variableDeclarator(ID, null)]),
+        ...(intermediate ? [intermediate] : []),
         t.ifStatement(
-          IS_ARRAY,
+          isArr,
           t.blockStatement([
             t.ifStatement(
-              t.binaryExpression('>=', INDEX,
-                t.memberExpression(LOOP_OBJECT, t.identifier('length'))),
+              t.binaryExpression('>=', index, memb(loopObj, ident('length'))),
               t.breakStatement()),
             t.expressionStatement(
-              t.assignmentExpression('=', ID,
-                t.memberExpression(
-                  LOOP_OBJECT,
-                  t.updateExpression('++', INDEX, false),
-                  true))),
+              t.assignmentExpression(
+                '=',
+                id,
+                memb(
+                  loopObj,
+                  t.updateExpression('++', index, false),
+                  true))
+            )
           ], []),
           t.blockStatement([
             t.expressionStatement(
-              t.assignmentExpression('=', INDEX,
+              t.assignmentExpression(
+                '=',
+                index,
                 t.callExpression(
-                  t.memberExpression(LOOP_OBJECT, t.identifier('next')),
-                  [])
-              )),
+                  memb(loopObj, ident('next')), []))
+            ),
             t.ifStatement(
-              t.memberExpression(INDEX, t.identifier('done')),
+              memb(index, ident('done')),
               t.breakStatement()),
             t.expressionStatement(
-              t.assignmentExpression('=', ID,
-                t.memberExpression(INDEX, t.identifier('value'))
-              )),
-          ], [])),
+              t.assignmentExpression('=', id, memb(index, ident('value')))
+            )
+          ], []))
       ], []));
   }
 
-  function _ForOfStatementArray(path) {
-    let { node, scope } = path;
-    let nodes = [];
-    let right = node.right;
+  function pushComputedPropsLoose(path, file) {
+    const { node, scope, parent } = path;
+    const { left } = node;
+    let declar, id, intermediate;
 
-    if (!t.isIdentifier(right) || !scope.hasBinding(right.name)) {
-      let uid = scope.generateUidIdentifier('arr');
-      nodes.push(t.variableDeclaration('var', [
-        t.variableDeclarator(uid, right)
-      ]));
-      right = uid;
+    if (
+      t.isIdentifier(left) ||
+      t.isPattern(left) ||
+      t.isMemberExpression(left)
+    ) {
+      // for (i of test), for ({ i } of test)
+      id = left;
+      intermediate = null;
+    } else if (t.isVariableDeclaration(left)) {
+      // for (let i of test)
+      id = scope.generateUidIdentifier("ref");
+      declar = t.variableDeclaration(left.kind, [
+        t.variableDeclarator(left.declarations[0].id, t.identifier(id.name)),
+      ]);
+      intermediate = t.variableDeclaration("var", [
+        t.variableDeclarator(t.identifier(id.name)),
+      ]);
+    } else {
+      throw file.buildCodeFrameError(
+        left,
+        `Unknown node type ${left.type} in ForStatement`,
+      );
     }
 
-    let iterationKey = scope.generateUidIdentifier('i');
+    const iteratorKey = scope.generateUidIdentifier("iterator");
+    const isArrayKey = scope.generateUidIdentifier("isArray");
 
-    let loop = buildForOfArray({
-      BODY: node.body,
-      KEY:  iterationKey,
-      ARR:  right
+    const loop = buildForOfLoose({
+      loopObj: iteratorKey,
+      isArr: isArrayKey,
+      obj: node.right,
+      index: scope.generateUidIdentifier("i"),
+      id: id,
+      intermediate: intermediate,
     });
 
-    t.inherits(loop, node);
-    t.ensureBlock(loop);
+    const isLabeledParent = t.isLabeledStatement(parent);
+    let labeled;
 
-    let iterationValue = t.memberExpression(right, iterationKey, true);
-
-    let left = node.left;
-    if (t.isVariableDeclaration(left)) {
-      left.declarations[0].init = iterationValue;
-      loop.body.body.unshift(left);
-    } else {
-      loop.body.body.unshift(t.expressionStatement(t.assignmentExpression('=', left, iterationValue)));
+    if (isLabeledParent) {
+      labeled = t.labeledStatement(parent.label, loop);
     }
 
-    if (path.parentPath.isLabeledStatement()) {
-      loop = t.labeledStatement(path.parentPath.node.label, loop);
-    }
-
-    nodes.push(loop);
-
-    return nodes;
+    return {
+      replaceParent: isLabeledParent,
+      declar: declar,
+      node: labeled || loop,
+      loop: loop,
+    };
   }
 
+
   return {
+    name: "transform-for-of",
     visitor: {
       ForOfStatement(path, state) {
-        if (path.get('right').isArrayExpression()) {
-          return path.replaceWithMultiple(_ForOfStatementArray.call(this, path, state));
-        }
-
-        let { node, scope } = path;
-
-        let left = node.left;
-        let declar, id;
-
-        if (t.isIdentifier(left) || t.isPattern(left) || t.isMemberExpression(left)) {
-          // for (i of test), for ({ i } of test)
-          id = left;
-        } else if (t.isVariableDeclaration(left)) {
-          // for (let i of test)
-          id = scope.generateUidIdentifier('ref');
-          declar = t.variableDeclaration(left.kind, [
-            t.variableDeclarator(left.declarations[0].id, id)
-          ]);
-        } else {
-          throw new Error(`Unknown node type ${left.type} in ForStatement`);
-        }
-
-        let iteratorKey = scope.generateUidIdentifier('iterator');
-        let isArrayKey  = scope.generateUidIdentifier('isArray');
-
-        let loop = buildForOfLoose({
-          LOOP_OBJECT:  iteratorKey,
-          IS_ARRAY:     isArrayKey,
-          OBJECT:       node.right,
-          INDEX:        scope.generateUidIdentifier('i'),
-          ID:           id
-        });
-
-        if (!declar) {
-          // no declaration so we need to remove the variable declaration at the top of
-          // the for-of-loose template
-          loop.body.body.shift();
-        }
-
-        let block  = loop.body;
+        const { node } = path;
+        const build = pushComputedPropsLoose(path, state);
+        const declar = build.declar;
+        const loop = build.loop;
+        const block = loop.body;
 
         // ensure that it's a block so we can take all its statements
         path.ensureBlock();
@@ -167,8 +138,13 @@ export default function transformForOfPlugin({types: t}) {
         t.inherits(loop, node);
         t.inherits(loop.body, node.body);
 
-        path.replaceWithMultiple(loop);
-      }
-    }
+        if (build.replaceParent) {
+          path.parentPath.replaceWithMultiple(build.node);
+          path.remove();
+        } else {
+          path.replaceWithMultiple(build.node);
+        }
+      },
+    },
   };
 }
---
+++ tests/test.for-of.js
@@ -0,0 +1,221 @@
+var _compile;
+
+if (typeof window === 'object') {
+  _compile = window.pregenerator.compile;
+  window.assert = window.chai.assert;
+  window.expect = window.chai.expect;
+} else {
+  _compile = require('pregenerator/test').compile;
+  var chai = require('chai');
+  global.assert = chai.assert;
+  global.expect = chai.expect;
+  var sinonChai = require('sinon-chai');
+  chai.use(sinonChai);
+  global.sinon = require('sinon');
+}
+
+function compile(src) {
+  return _compile(src, {allowReturnOutsideFunction: true});
+}
+
+describe('for-of', function() {
+  it('simple array', function() {
+    eval(compile([
+      'let elm;',
+      'let arr = [0, 1, 2];',
+      'let res = [];',
+      '',
+      'for (elm of arr) {',
+      '  res.push(elm);',
+      '}',
+      '',
+      'assert.deepEqual(res, arr);',
+      ''
+    ].join('\n')));
+  });
+
+  it('array pattern', function() {
+    eval(compile([
+      'let elm;',
+      'let arr = [[0], [1], [2]];',
+      'let res = [];',
+      'for ([elm] of arr) {',
+      '  res.push(elm);',
+      '}',
+      'assert.deepEqual(res, [0, 1, 2]);',
+      ''
+    ].join('\n')));
+  });
+
+  it('declaration', function() {
+    eval(compile([
+      'let arr = [0, 1, 2];',
+      'let res = [];',
+      '',
+      'for (const elm of arr) {',
+      '  res.push(elm);',
+      '}',
+      '',
+      'assert.deepEqual(res, [0, 1, 2]);',
+      ''
+    ].join('\n')));
+  });
+
+  it('declaration array pattern', function() {
+    eval(compile([
+      'let arr = [[0], [1], [2]];',
+      'let res = [];',
+      '',
+      'for (const [elm] of arr) {',
+      '  res.push(elm);',
+      '}',
+      '',
+      'assert.deepEqual(res, [0, 1, 2]);',
+      ''
+    ].join('\n')));
+  });
+
+  it('expression', function() {
+    eval(compile([
+      'let arr = [0, 1, 2];',
+      'let res = [];',
+      '',
+      'let i;',
+      'for (i of arr) res.push(i);',
+      '',
+      'assert.deepEqual(res, [0, 1, 2]);',
+      ''
+    ].join('\n')));
+  });
+
+  it('expression declaration', function() {
+    eval(compile([
+      'let arr = [0, 1, 2];',
+      'let res = [];',
+      'let i;',
+      'for (const i of arr) res.push(i);',
+      'assert.deepEqual(res, [0, 1, 2]);',
+      ''
+    ].join('\n')));
+  });
+
+  it('redeclare', function() {
+    eval(compile([
+      'let arr = [0, 1, 2];',
+      'let res = [];',
+      '',
+      'for (let o of arr) {',
+      '  const arr = o;',
+      '  res.push(o);',
+      '}',
+      '',
+      'assert.deepEqual(res, [0, 1, 2]);',
+      ''
+    ].join('\n')));
+  });
+
+  it('static', function() {
+    eval(compile([
+      'let elm;',
+      'const arr = [0, 1, 2];',
+      'let res = [];',
+      '',
+      'for (elm of arr) {',
+      '  res.push(elm);',
+      '}',
+      '',
+      'assert.deepEqual(res, [0, 1, 2]);',
+      ''
+    ].join('\n')));
+  });
+
+  it('static declaration', function() {
+    eval(compile([
+      'const arr = [0, 1, 2];',
+      'let res = [];',
+      '',
+      'for (const elm of arr) {',
+      '  res.push(elm);',
+      '}',
+      '',
+      'assert.deepEqual(res, [0, 1, 2]);',
+      'assert.equal(typeof elm, \'undefined\');',
+      ''
+    ].join('\n')));
+  });
+
+  it('ignore cases', function() {
+    eval(compile([
+      'var foo = [0, 1, 2];',
+      'var res = [];',
+      '',
+      'for (var i of foo) {',
+      '  res.push(i);',
+      '  switch (i) {',
+      '    case 1:',
+      '      break;',
+      '  }',
+      '}',
+      '',
+      'assert.deepEqual(res, [0, 1, 2]);',
+      ''
+    ].join('\n')));
+  });
+
+  it('member expression', function() {
+    eval(compile([
+      'const obj = {};',
+      'let arr = [0, 1, 2];',
+      'let res = [];',
+      'for (obj.prop of arr) {',
+      '  res.push(obj.prop);',
+      '}',
+      'assert.equal(obj.prop, 2);',
+      'assert.deepEqual(res, [0, 1, 2]);',
+      ''
+    ].join('\n')));
+  });
+
+  it('multiple', function() {
+    eval(compile([
+      'var arr = [0, 1, 2];',
+      'var nums = [3, 4, 5];',
+      'var res = [];',
+      '',
+      'for (var i of arr) {',
+      '  res.push(i);',
+      '}',
+      '',
+      'for (var i of nums) {',
+      '  res.push(i);',
+      '}',
+      '',
+      'assert.deepEqual(res, [0, 1, 2, 3, 4, 5]);',
+      ''
+    ].join('\n')));
+  });
+
+  it('nested label for-of', function() {
+    eval(compile([
+      'function d() {',
+      '  return ["foo", "bar"];',
+      '}',
+      'function f() {',
+      '  return [1];',
+      '}',
+      '',
+      'var res = [];',
+      '',
+      'b: for (let c of d()) {',
+      '  res.push(c);',
+      '  for (let e of f()) {',
+      '    continue b;',
+      '  }',
+      '  res.push("-");',
+      '}',
+      '',
+      'assert.deepEqual(res, ["foo", "bar"]);',
+      ''
+    ].join('\n')));
+  });
+});
